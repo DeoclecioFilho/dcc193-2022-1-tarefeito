afd67c3db6049dafb84356eca0eac6788aa4dd6c
Deoclecio <deoclecio.filho@estudante.ufjf.br>
2022-05-31 21:38:19 -0300

    Crio o projeto novo

---
+++ src/test/java/br/dcc/ufjf/deocleciofilho/tarefeito/TarefeitoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dcc.ufjf.deocleciofilho.tarefeito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefeitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
