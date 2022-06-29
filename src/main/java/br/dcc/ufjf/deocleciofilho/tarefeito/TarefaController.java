356e6c28ef15231270ccd8c5540e423f0194f2e0
Deoclecio <deoclecio.filho@estudante.ufjf.br>
2022-06-28 21:55:08 -0300

    Adicionado template

--- src/main/java/br/dcc/ufjf/deocleciofilho/tarefeito/TarefaController.java
+++ src/main/java/br/dcc/ufjf/deocleciofilho/tarefeito/TarefaController.java
@@ -7,16 +7,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.engine.AttributeName;
+
+import aj.org.objectweb.asm.Attribute;
+
+
 
 @Controller
 public class TarefaController {
     @Autowired
-    private TarefaRepositorio rep;
+    TarefaRepositorio rep;
 
 
     @RequestMapping("/")
     @ResponseBody
-    public String index(){
+    public String index2(){
         Tarefa t = new Tarefa("Criado em "+new Date());
         rep.save(t);
         StringBuilder sb =new StringBuilder();
@@ -29,4 +35,13 @@ public class TarefaController {
        
         return  sb.toString();
     }
+
+    @RequestMapping({"/tarefa", "/tarefa/index.html"})
+    
+    public ModelAndView index() {
+        ModelAndView mv =new ModelAndView("tarefa-index");
+        mv.addObject("mensagem", "Ola Mundo!");
+        return mv;
+    }
+    
 }
