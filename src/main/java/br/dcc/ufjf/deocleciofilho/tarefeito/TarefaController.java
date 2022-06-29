e1a29df367670208484827dfba7c0137505a643a
Deoclecio <deoclecio.filho@estudante.ufjf.br>
2022-06-28 22:48:45 -0300

    Adcionou listagem

--- src/main/java/br/dcc/ufjf/deocleciofilho/tarefeito/TarefaController.java
+++ src/main/java/br/dcc/ufjf/deocleciofilho/tarefeito/TarefaController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.thymeleaf.engine.AttributeName;
@@ -15,10 +16,10 @@ import aj.org.objectweb.asm.Attribute;
 
 
 @Controller
+@RequestMapping("/tarefa")
 public class TarefaController {
     @Autowired
     TarefaRepositorio rep;
-
 
     @RequestMapping("/")
     @ResponseBody
@@ -36,12 +37,38 @@ public class TarefaController {
         return  sb.toString();
     }
 
-    @RequestMapping({"/tarefa", "/tarefa/index.html"})
+    @RequestMapping({"/index.html"})
     
     public ModelAndView index() {
         ModelAndView mv =new ModelAndView("tarefa-index");
         mv.addObject("mensagem", "Ola Mundo!");
         return mv;
+    }
+
+    @RequestMapping(path = "/nova.html", method = RequestMethod.GET)
+    public ModelAndView novaGET(){
+        ModelAndView mv = new ModelAndView("tarefa-new");
+        Tarefa t = new Tarefa("Criado em " + new Date());
+        mv.addObject("tarefa",t);
+        return mv;
+    }
+
+    @RequestMapping(path = "/nova.html", method = RequestMethod.POST)
+    public ModelAndView novaPOST(Tarefa t){
+        ModelAndView mv = new ModelAndView();
+        rep.save(t);
+        mv.addObject("tarefa", t);
+        mv.setViewName("redirect:listar.html");
+        return mv;       
+    }
+
+    @RequestMapping(path = "/listar.html")
+    public ModelAndView listar(){
+        ModelAndView mv = new ModelAndView("tarefa-list");
+        List<Tarefa> tl = rep.findAll();
+        mv.addObject("tarefas",tl);
+        return mv;
+
     }
     
 }
