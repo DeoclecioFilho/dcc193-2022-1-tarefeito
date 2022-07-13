1b360839226d5272c9ce2712b8d68a96e0782c8f
Deoclecio <deoclecio.filho@estudante.ufjf.br>
2022-07-12 21:59:43 -0300

    Altera para Arround

--- src/main/java/br/dcc/ufjf/deocleciofilho/tarefeito/aspecto/LogAspect.java
+++ src/main/java/br/dcc/ufjf/deocleciofilho/tarefeito/aspecto/LogAspect.java
@@ -2,19 +2,41 @@ package br.dcc.ufjf.deocleciofilho.tarefeito.aspecto;
 
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class LogAspect {
+    /* 
     @Before("execution(* *..*.*Controller.*(..))")
     private void antesLog(JoinPoint jp){
         System.out.println("O metodo vai ser chamado: "+jp.getSignature());
     }
+*/
+    @Around("execution(* *..*.*Controller.*(..))")
+    
+    private Object antesLog(ProceedingJoinPoint jp) throws Throwable{
+       
+        System.out.println("O metodo vai ser chamado: "+jp.getSignature());
+        Object retorno;
+        try{
+        retorno = jp.proceed();    
+        }catch(Exception e){
+            System.out.println("O metodo vai ser chamado: "+jp.getSignature());
+            throw e;
+        }finally{
+            System.out.println("O metodo vai ser chamado: "+jp.getSignature());
+        }
+        return retorno;
+    }
+
     @After("execution(* *..*.*Controller.*(..))")
     private void depoisLog(JoinPoint jp){
         System.out.println("O metodo terminou: "+jp.getSignature());
     }
+
 }
